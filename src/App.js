fad8a715b63d66eec255ce21051be86e3f664072
avish <avishusa45@gmail.com>
2024-07-30 02:41:15 -0500

    Implemeted different types of routing using react-router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes, Navigate, useLocation } from "react-router-dom";
+import React, { useState, Suspense } from "react";
+import { SwitchTransition, CSSTransition } from "react-transition-group";
+import { appRoutes } from "./routes";
+
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [isLogged, setIsLogged] = useState(false);
+  const location = useLocation();
   return (
-    
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    
+    <SwitchTransition component={null}>
+      <CSSTransition
+        key={location.pathname}
+        classNames="fade"
+        timeout={300}
+        unmountOnExit
+      >
+        <Suspense fallback={() => <h1>Loading...</h1>}>
+          <Routes location={location}>
+            {appRoutes.map((route) => {
+              if (route.requiresAuth && !isLogged) {
+                return (
+                  <Route
+                    key={route.path}
+                    exact
+                    path={route.path}
+                    element={<Navigate replace to={"/login"} />}
+                  />
+                );
+              } else {
+                return (
+                  <Route
+                    key={route.path}
+                    exact
+                    path={route.path}
+                    element={
+                      <route.component
+                        setIsLogged={setIsLogged}
+                        setUsername={setUsername}
+                        username={username}
+                      />
+                    }
+                  />
+                );
+              }
+            })}
+          </Routes>
+        </Suspense>
+      </CSSTransition>
+    </SwitchTransition>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter  } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/navbar";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <Navbar />
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
